92f72da1a394a87e71eb91693b4b101b23dec773
Greg Stead <gregstead@gmail.com>
2017-02-13 18:40:06 -0600

    I'm bad at saving

--- main.py
+++ main.py
@@ -14,12 +14,71 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Post(db.Model):
+
+    title = db.StringProperty(required=True)
+    body = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self,*a,**kw):
+        self.response.out.write(*a,**kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **params):
+        self.write(self.render_str(template,**params))
+
+class MainPage(Handler):
+    def render_main(self, title="", body="",error=""):
+
+        self.render("main.html",title=title, body=body,error=error)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render("main.html")
+
+    def post(self):
+        title = self.request.get("title")
+        body = self.request.get("body")
+
+        error = "We need a title and content!"
+
+        if title and body:
+            p = Post(title=title,body=body)
+            p.put()
+
+            self.redirect('/blog')
+
+        else:
+            self.render_main(title,body,error)
+
+class BlogHandler(Handler):
+
+    def get(self):
+
+        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")
+
+        self.render("blog.html", posts=posts)
+
+    def post(self):
+
+        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")
+
+        self.render("blog.html", posts=posts)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage),
+    ('/blog', BlogHandler),
 ], debug=True)
